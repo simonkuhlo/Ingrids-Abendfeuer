a9eaaf04616a0f2ddb7af4bb1a59342dac81d638
Simon <siga241@gmail.com>
2025-02-11 13:42:17 +0100

    [ #3 ] Fix Coin Tracker

--- UI/coin_tracker.gd
+++ UI/coin_tracker.gd
@@ -3,18 +3,23 @@ extends Label
 @export var inventory:PlayerInventory
 @export var active:bool = false
 
+@export var coin_item:GameItem
+
 var old_amount:int = 0
 
-#TODO Make this work with coins as inventoryitem
-
 func _ready():
-	pass
 	if active:
-		self.text = "Coins: " + str(inventory.coins)
+		self.text = "Coins: " + str(get_current_coin_amount())
 
 func _process(delta):
-	pass
 	if active:
-		if inventory.coins != old_amount:
-			self.text = "Coins: " + str(inventory.coins)
-			old_amount = inventory.coins
+		var coins = get_current_coin_amount()
+		if coins != old_amount:
+			self.text = "Coins: " + str(coins)
+			old_amount = coins
+
+func get_current_coin_amount() -> int:
+	var item_container:GameItemContainer = inventory.get_item_container(coin_item)
+	if item_container:
+		return item_container.amount
+	return 0
