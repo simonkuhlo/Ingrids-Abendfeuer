42ae24a3abc9748221b630661f66a1451b81b613
Simon <siga241@gmail.com>
2025-02-13 14:04:12 +0100

    Add cast_mode to skills

--- Skills/skill_resource.gd
+++ Skills/skill_resource.gd
@@ -5,4 +5,7 @@ class_name SkillResource
 @export var cooldown:float
 @export var costs:Array[PlayerResourceCost]
 
+enum CastMode {ONESHOT, TOGGLE, CONTINUOUS}
+@export var cast_mode:CastMode = CastMode.ONESHOT
+
 @export var instance:PackedScene
