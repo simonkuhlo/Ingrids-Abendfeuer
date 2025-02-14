dc45ff265eac53dba253fa8edce7af50398c926a
Simon <siga241@gmail.com>
2025-02-14 10:32:53 +0100

    [ #12 ] Change skill system structure

--- Skills/skill_instance.gd
+++ Skills/skill_instance.gd
@@ -1,8 +1,47 @@
 extends Node
 class_name SkillInstance
 
-var caster:Player
+@export var owning_player:Player
+@export var trigger:StringName
+@export var resource:SkillResource
+
+## The current cooldown of the Spell
+var cooldown_timer:Timer = Timer.new()
+var cast_timer:Timer = Timer.new()
 
 
-func end_skill():
-	queue_free()
+enum ReadyState {READY, ON_COOLDOWN, NO_RESOURCE, BLOCKED, CASTING}
+## If the Spell currently is ready to cast or not
+var ready_state:ReadyState = ReadyState.BLOCKED
+
+func ready() -> void:
+	cooldown_timer.autostart = false
+	cooldown_timer.one_shot = true
+	cast_timer.autostart = false
+	cast_timer.one_shot = true
+	add_child(cooldown_timer)
+	add_child(cast_timer)
+	if resource:
+		cooldown_timer.wait_time = resource.cooldown
+		cast_timer.wait_time = resource.cooldown
+
+func _physics_process(delta: float) -> void:
+	ready_state = get_ready_state()
+	if ready_state == ReadyState.READY:
+		if Input.is_action_just_pressed(trigger):
+			cast_skill()
+
+func get_ready_state() -> ReadyState:
+	if !cooldown_timer.is_stopped():
+		return ReadyState.ON_COOLDOWN
+	else:
+		for cost in resource.costs:
+			var player_resource:PlayerResourceInstance = owning_player.resources.get_resource(cost.resource)
+			if !player_resource:
+				return ReadyState.NO_RESOURCE
+			if resource.amount < cost.cost:
+				return ReadyState.NO_RESOURCE
+		return ReadyState.READY
+
+func cast_skill():
+	owning_player.motion_machine.apply_force(Vector3.UP * 5)
--- Skills/skill_slot.gd
+++ Skills/skill_slot.gd
@@ -12,45 +12,9 @@ class_name SkillSlot
 ## The Player that owns this slot
 @export var player:Player
 
-## The current cooldown of the Spell
-var cooldown_timer:Timer = Timer.new()
-var cast_timer:Timer = Timer.new()
-
-
-enum ReadyState {READY, ON_COOLDOWN, NO_RESOURCE, BLOCKED, CASTING}
-## If the Spell currently is ready to cast or not
-var ready_state:ReadyState = ReadyState.BLOCKED
-
-func ready() -> void:
-	cooldown_timer.autostart = false
-	cooldown_timer.one_shot = true
-	cast_timer.autostart = false
-	cast_timer.one_shot = true
-	add_child(cooldown_timer)
-	add_child(cast_timer)
-	if resource:
-		cooldown_timer.wait_time = resource.cooldown
-		cast_timer.wait_time = resource.cooldown
-
-func _physics_process(delta: float) -> void:
-	ready_state = get_ready_state()
-	if ready_state == ReadyState.READY:
-		if Input.is_action_just_pressed(trigger):
-			cast_skill()
-
-func get_ready_state() -> ReadyState:
-	if !cooldown_timer.is_stopped():
-		return ReadyState.ON_COOLDOWN
-	else:
-		for cost in resource.costs:
-			var player_resource:PlayerResourceInstance = player.resources.get_resource(cost.resource)
-			if !player_resource:
-				return ReadyState.NO_RESOURCE
-			if resource.amount < cost.cost:
-				return ReadyState.NO_RESOURCE
-		return ReadyState.READY
-
-func cast_skill():
-	var skill_instance:SkillInstance = resource.instance.instantiate()
-	skill_instance.caster = player
-	player.skill_instances.add_child(skill_instance)
+func _ready():
+	var instance:SkillInstance = resource.instance.instantiate()
+	instance.owning_player = player
+	instance.trigger = trigger
+	instance.resource = resource
+	add_child(instance)
