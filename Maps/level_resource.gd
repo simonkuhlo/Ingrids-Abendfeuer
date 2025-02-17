825f93f866aa8ed7f63851577d96867f86962a08
Simon <siga241@gmail.com>
2025-02-17 22:07:15 +0100

    Add preview image to LevelResource

--- Maps/level_resource.gd
+++ Maps/level_resource.gd
@@ -3,3 +3,4 @@ class_name LevelResource
 
 @export var name:StringName
 @export var scene:PackedScene
+@export var preview_image:Texture2D
--- UI/MainMenu/level_selector.gd
+++ UI/MainMenu/level_selector.gd
@@ -1,7 +1,6 @@
 extends Control
 
 @export var listed_levels:Array[LevelResource] = []
-@export var level_button_scene:PackedScene
 
 @export var level_list:Container
 
@@ -10,7 +9,3 @@ func _ready():
 	for level in listed_levels:
 		var button_instance = LevelLoadButton.new(level)
 		level_list.add_child(button_instance)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
